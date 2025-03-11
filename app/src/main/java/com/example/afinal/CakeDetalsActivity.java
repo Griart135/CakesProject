0af1ad9d13e574635205d9993271532b23829a1a
Asus <@Griart135>
2025-03-11 16:14:32 +0400

    Order dzats

--- app/src/main/java/com/example/afinal/CakeDetalsActivity.java
+++ app/src/main/java/com/example/afinal/CakeDetalsActivity.java
@@ -1,38 +1,3 @@
-//package com.example.afinal;
-//
-//import android.os.Bundle;
-//import android.widget.ImageView;
-//import android.widget.LinearLayout;
-//import android.widget.TextView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//public class CakeDetalsActivity extends AppCompatActivity {
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_cake_details);
-//
-//        ImageView cakeImageView = findViewById(R.id.banana_for_pager);
-//        TextView cakeNameTextView = findViewById(R.id.cake_name);
-//        LinearLayout ingredientsLayout = findViewById(R.id.ingredients_layout);
-//
-//        int imageResId = getIntent().getIntExtra("imageResId", -1);
-//        String cakeName = getIntent().getStringExtra("cakeName");
-//        String cakeDescription = getIntent().getStringExtra("cakeDescription");
-//        String[] ingredientsList = getIntent().getStringArrayExtra("ingredients");
-//
-//        cakeImageView.setImageResource(imageResId);
-//        cakeNameTextView.setText(cakeName);
-//
-//        for (String ingredient : ingredientsList) {
-//            TextView ingredinetTextView = new TextView(this);
-//            ingredinetTextView.setText(ingredient);
-//            ingredinetTextView.setTextSize(16);
-//            ingredientsLayout.addView(ingredinetTextView);
-//        }
-//    }
-//}
 package com.example.afinal;
 
 import android.content.Intent;
