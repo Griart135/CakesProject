9b1ad00bec818f2c6a1222244d6eae91cce10b95
Asus <@Griart135>
2025-03-25 12:03:03 +0400

    numberPicker

--- app/src/main/java/com/example/afinal/OrderActivity.java
+++ app/src/main/java/com/example/afinal/OrderActivity.java
@@ -3,15 +3,21 @@ package com.example.afinal;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderActivity extends AppCompatActivity {
 
-    private TextView heightLabel, radiusLabel;
+    private TextView heightLabel, radiusLabel, slicesLabel;
     private SeekBar heightSeekBar, radiusSeekBar;
+    private NumberPicker slicesPicker;
+    private Button orderSlicesButton;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,7 +31,26 @@ public class OrderActivity extends AppCompatActivity {
         radiusSeekBar = findViewById(R.id.order_radius_seekbar);
         ImageView orderCakeImage = findViewById(R.id.cake_image);
         TextView cakeNameTextView = findViewById(R.id.cake_name);
+        slicesPicker = findViewById(R.id.order_slices_picker);
+        orderSlicesButton = findViewById(R.id.order_slices_button);
+        slicesLabel = findViewById(R.id.slices_label);
 
+
+
+        slicesPicker.setMinValue(1);
+        slicesPicker.setMaxValue(12);
+        slicesPicker.setWrapSelectorWheel(true);
+
+        slicesPicker.setOnValueChangedListener((picker, oldVal, newVal) ->
+                slicesLabel.setText("Куски: " + newVal)
+        );
+
+
+
+        orderSlicesButton.setOnClickListener(v -> {
+            int selectedSlices = slicesPicker.getValue();
+            Toast.makeText(OrderActivity.this, "Вы выбрали " + selectedSlices + " кусочков", Toast.LENGTH_SHORT).show();
+        });
 
         int imageResId = getIntent().getIntExtra("ImageResId", -1);
         String cakeName = getIntent().getStringExtra("cake_name");
@@ -42,13 +67,15 @@ public class OrderActivity extends AppCompatActivity {
         }
 
         heightSeekBar.setProgress(10);
-        radiusSeekBar.setProgress(50);
+        radiusSeekBar.setProgress(20);
+
+        heightSeekBar.setMax(45);
+        radiusSeekBar.setMax(25);
 
         heightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                double height = Math.round(progress / 10.0 * 10.0) / 10.0;
-                heightLabel.setText("Height: " + height + " cm");
+                heightLabel.setText("Height: " + (progress + 5) + " cm");
             }
 
             @Override
@@ -61,8 +88,7 @@ public class OrderActivity extends AppCompatActivity {
         radiusSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                double radius = Math.round(progress / 10.0 * 10.0) / 10.0;
-                radiusLabel.setText("Radius: " + radius + " cm");
+                radiusLabel.setText("Radius: " + (progress + 5) + " cm");
             }
 
             @Override
@@ -70,6 +96,13 @@ public class OrderActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+        Button orderButton = findViewById(R.id.order_button);
+        orderButton.setOnClickListener(v -> {
+            int height = heightSeekBar.getProgress() + 5;
+            int radius = radiusSeekBar.getProgress() + 5;
+
+            Toast.makeText(OrderActivity.this, "Вы заказали торт с высотой " + height + " см и радиусом " + radius + " см", Toast.LENGTH_SHORT).show();
         });
     }
 }
